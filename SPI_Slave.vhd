2bf2dd3594bc816bb2e1dfeb1fdc8c36421a0fbb
Derek <dschacht@gmail.com>
2015-10-01 01:52:53 -0400

    Updated the SPI_Slave module and ran simulation all the way through layout. Next will be to try it on a device.

--- SPI_Slave.vhd
+++ SPI_Slave.vhd
@@ -34,8 +34,6 @@ entity SPI_Slave is
           SPI_Enable        : in    SPI_Bit_Type;
           Data_To_Transmit  : in    SPI_Data_Type;
           Data_Received     :   out SPI_Data_Type;
-          Count_Port        :   out SPI_Bit_Count_Type;
-          State_Port        :   out SPI_State_Type;
           SPI_Status        :   out SPI_Status_Array_Type);
 
 end SPI_Slave;
@@ -43,9 +41,41 @@ end SPI_Slave;
 
 architecture architecture_SPI_Slave of SPI_Slave is
 
+    signal SPI_Clock_Net  : SPI_Bit_Type;
+    signal SPI_MOSI_Net   : SPI_Bit_Type;
+    signal SPI_Enable_Net : SPI_Bit_Type;
+
 begin
 
-    process (Clock)
+    -- Alias the SPI clock so that it is stable during a system clock period.
+    -- Alias the SPI MOSI pin so that it is stable during a system clock period.
+    -- Alias the SPI Enable pin so that it is stable during a system clock period.
+
+    Input_Alias : process (Clock)
+
+        variable SPI_Clock_Alias  : SPI_Bit_Type;
+        variable SPI_MOSI_Alias   : SPI_Bit_Type;
+        variable SPI_Enable_Alias : SPI_Bit_Type;
+
+    begin
+
+        SPI_Clock_Net  <= SPI_Clock_Alias;
+        SPI_MOSI_Net   <= SPI_MOSI_Alias;
+        SPI_Enable_Net <= SPI_Enable_Alias;
+
+        if rising_edge (Clock) then
+
+            SPI_Clock_Alias  := SPI_Clock;
+            SPI_MOSI_Alias   := SPI_MOSI;
+            SPI_Enable_Alias := SPI_Enable;          
+
+        end if;
+
+    end process;
+
+
+
+    SPI_Slave_State_Machine : process (Clock)
 
         variable SPI_State      : SPI_State_Type;
         variable SPI_Data       : SPI_Data_Type;
@@ -54,9 +84,6 @@ begin
         variable Count          : SPI_Bit_Count_Type;
 
     begin
-    
-        Count_Port    <= Count;
-        State_Port    <= SPI_State;
 
         SPI_Status    <= SPI_Status_Reg;
         Data_Received <= SPI_Data_In;
@@ -65,22 +92,27 @@ begin
 
             if Reset = SPI_Reset_Polarity then
 
+                Count := 0;
+
                 SPI_State              := Wait_State;
-                SPI_Data               := (others => SPI_MISO_Default);
-                SPI_Data_In            := (others => SPI_MISO_Default);
+                SPI_Data               := (others => '0');
+                SPI_Data_In            := (others => '0');
                 SPI_MISO               <= SPI_MISO_Default;
+
                 SPI_Status_Reg(Ready)      := SPI_Status_Polarity;
                 SPI_Status_Reg(Data_Ready) := not SPI_Status_Polarity;
                 SPI_Status_Reg(Error)      := not SPI_Status_Polarity;
 
-            elsif SPI_Enable = not SPI_Enable_Polarity then
+            elsif SPI_Enable_Net = not SPI_Enable_Polarity then
+
+                -- If the SPI Slave module is not enabled reset the module to a good
+                -- starting condition for when the line does drop low.
 
                 SPI_State              := Wait_State;
-                SPI_Data               := (others => SPI_MISO_Default);
+                SPI_Data               := (others => '0');
                 SPI_MISO               <= SPI_MISO_Default;
 
                 SPI_Status_Reg(Ready)      := SPI_Status_Polarity;
-                SPI_Status_Reg(Data_Ready) := not SPI_Status_Polarity;
                 SPI_Status_Reg(Error)      := not SPI_Status_Polarity;
 
             else
@@ -89,26 +121,26 @@ begin
 
                     when Wait_State =>
 
-                        if SPI_Enable = SPI_Enable_Polarity then
+                        -- Leave the wait state if the enable polarity is active Techically this
+                        -- check is not needed since the above elsif is guaranteed to trap the 
+                        -- state of the SPI Slave module if the SPI_Enable signal is the inactive.
 
-                            SPI_Data              := Data_To_Transmit;
-                            SPI_Status_Reg(Ready) := not SPI_Status_Polarity;
-                            Count                 := SPI_Data_MSB;
+                        --if SPI_Enable_Net = SPI_Enable_Polarity then
 
-                            SPI_State         := Enable_State;
-
-                        else
-    
-                            SPI_Status_Reg(Ready)      := SPI_Status_Polarity;
-                            SPI_Status_Reg(Data_Ready) := not SPI_Status_Polarity;
-                            SPI_Status_Reg(Error)      := not SPI_Status_Polarity;
-                            SPI_MISO <= SPI_MISO_Default;
                             
-                        end if;
+                            SPI_Status_Reg(Ready) := not SPI_Status_Polarity;
+
+                            SPI_State             := Enable_State;
+                            
+                        --end if;
 
                     when Enable_State =>
 
-                        if SPI_Clock = not SPI_Clock_Polarity then
+                        -- Leave the enable state if the clock is low.
+                        Count                 := SPI_Data_MSB;
+                        SPI_Data              := Data_To_Transmit;
+
+                        if SPI_Clock_Net = not SPI_Clock_Polarity then
 
                             SPI_State                  := Setup_State;
                             SPI_Status_Reg(Data_Ready) := not SPI_Status_Polarity;
@@ -117,44 +149,55 @@ begin
 
                     when Setup_State =>
 
-                        if SPI_Clock = SPI_Clock_Polarity then
+                        -- Leave the setup state if the clock is high. This is also where
+                        -- the input data is sampled. Output data is set when the clock is
+                        -- low.
 
-                            SPI_State := Data_State;
+                        if SPI_Clock_Net = SPI_Clock_Polarity then
+
+                            SPI_State      := Data_State;
+                            SPI_Data_In(0) := SPI_MOSI_Net;
 
                         else
 
-                            SPI_MISO <= SPI_Data(Count);
+                            SPI_MISO <= SPI_Data(7);
 
                         end if;
 
                     when Data_State =>
 
-                        if SPI_Clock = not SPI_Clock_Polarity then
+                        -- Leave the data state for the stop state if all of the data has
+                        -- been received. Otherwise wait for the clock to fall, update the 
+                        -- data registers as part of moving back to the setup state.
 
-                            if Count > 0 then
+                        if Count = 0 then
 
-                                SPI_State := Setup_State;
-                                Count     := Count - 1;
+                            SPI_State   := Stop_State;
 
-                            end if;
+                        elsif SPI_Clock_Net = not SPI_Clock_Polarity then
 
-                        else
-
-                            if Count = 0 then
-
-                                SPI_State   := Stop_State;
-                                
-                            end if;
-
-                            SPI_Data(Count) := SPI_MOSI;
+                            SPI_State := Setup_State;
+                            Count     := Count - 1;
+                            SPI_Data(7 downto 1)    := SPI_Data(6 downto 0);
+                            SPI_Data_In(7 downto 1) := SPI_Data_In(6 downto 0);
 
                         end if;
 
                     when Stop_State =>
 
-                        SPI_Data_In                := SPI_Data;
+                        -- Set the data ready flag and go back to the enable state when 
+                        -- the SPI clock is high.
+
                         SPI_Status_Reg(Data_Ready) := SPI_Status_Polarity;
-                        SPI_State                  := Wait_State;
+
+                        -- This check is not needed either since the data state is during
+                        -- the high clock cycle.
+
+                        --if SPI_Clock_Net = SPI_Clock_Polarity then
+
+                            SPI_State := Enable_State;
+
+                        --end if;
 
                 end case;
 
